e05d700e822d3d19f6d7cb063c9e1b3e0e8161a3
maissane_ba <bakri.maissane@gmail.com>
2025-08-22 20:47:56 +0200

    attempt to fix auth error 2

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { createServer } from 'http';
 import app from './app';
 
-
 const server = createServer(app);
 const port = process.env.PORT || 3000;
 
@@ -10,9 +9,3 @@ server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Server closed');
-    process.exit(0);
-  });
-});
